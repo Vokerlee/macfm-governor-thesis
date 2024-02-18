442a776ab975f10a7051ec9cfcd51bf8b33a14fe
Vokerlee <vokerlee@gmail.com>
2024-02-18 16:10:19 +0300

    Add Arch Linux and Ubuntu packages' installation scripts

--- scripts/packages/distr/arch.sh
+++ scripts/packages/distr/arch.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo "Not implemented!"
+packages_list="base-devel git m4 scons clang gcc protobuf boost \
+               curl python3 graphviz aarch64-linux-gnu-gcc      \
+               python-graphviz"
+
+sudo pacman --noconfirm -Sy ${packages_list}
--- scripts/packages/distr/ubuntu.sh
+++ scripts/packages/distr/ubuntu.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "Not implemented!"
+packages_list="build-essential git m4 scons zlib1g zlib1g-dev                \
+    libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev  \
+    python3-dev libboost-all-dev pkg-config python3-tk gcc-aarch64-linux-gnu \
+    python-pydot python-pydot-ng graphviz"
+
+sudo apt update
+sudo apt -y install ${packages_list}
