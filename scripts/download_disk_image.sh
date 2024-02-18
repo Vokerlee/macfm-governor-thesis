d4b5aa2e4612085683cc3a09259e8e15ad5b1620
Vokerlee <vokerlee@gmail.com>
2024-02-18 19:37:51 +0300

    Remove disk_images folder in scripts

--- scripts/download_disk_image.sh
+++ scripts/download_disk_image.sh
@@ -8,7 +8,7 @@ fs_image_name_bz2=${fs_image_name}".bz2"
 
 script_path=$(dirname $(realpath $0))
 fs_images_folder="fs_images"
-fs_images_path=${script_path}/../../${fs_images_folder}
+fs_images_path=$(realpath ${script_path}/../../${fs_images_folder})
 
 mkdir -p ${fs_images_path}
 gsutil cp -r ${fs_image_src}/${fs_image_name_bz2} ${fs_images_path}
